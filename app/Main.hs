407a72fc8e9952341143d73053c0de79c1c86ef8
H. Gabriel Heady <headyhud@gmail.com>
2020-12-10 07:57:39 -0800

    FRUITBAR-0: Populate sqlite in memory

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,48 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Main where
 
 import Lib
 import Control.Applicative
 import Control.Monad (mzero)
+import Control.Monad.IO.Class  (liftIO)
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import Data.Time
 import Data.Time.Format.ISO8601
 import Data.Time (UTCTime)
 import qualified Data.Vector as V
+import           Database.Persist
+import           Database.Persist.Sqlite
+import           Database.Persist.TH
 
 -- main :: IO ()
 -- main = someFunc
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person
+    name String
+    deriving Show
+ConsumptionDB
+    personId PersonId
+    meat String
+    time UTCTime
+|]
 
 data Consumption = Consumption
     { name :: !String
@@ -32,9 +60,13 @@ instance FromNamedRecord Consumption where
       Nothing -> fail "Invalid timestamp"
 
 main :: IO ()
-main = do
-    csvData <- BL.readFile "data.csv"
-    case decodeByName csvData of
-        Left err -> putStrLn err
-        Right (_, v) -> V.forM_ v $ \ p ->
-            putStrLn $ name p ++ " ate " ++ show (bar p) ++ " on " ++ show (time p)
+main = runSqlite ":memory:" $ do
+  runMigration migrateAll
+  csv <- liftIO $ BL.readFile "data.csv"
+  _ <- case decodeByName csv of
+    Left err -> liftIO $ putStrLn err
+    Right (_, v) -> V.forM_ v $ \ p -> do
+      pid <- insert $ Person $ name p
+      _   <- insert $ ConsumptionDB pid (bar p) (time p)
+      liftIO $ putStrLn "done"
+  pure ()
