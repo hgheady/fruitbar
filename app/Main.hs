10bda45169adc4cc9e0c53a65c2566f1b4151802
H. Gabriel Heady <headyhud@gmail.com>
2020-12-12 08:39:13 -0800

    FRUITBAR-0: Add /consumptions
    TODO: endpoints should return objects, currently they return bare lists

--- app/Main.hs
+++ app/Main.hs
@@ -16,42 +16,69 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE UndecidableInstances #-}
 
+-- Generic ToJSON
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
 module Main where
 
 -- import Lib
-import Control.Monad.IO.Class  (liftIO, MonadIO(..))
+-- import Control.Monad.IO.Class  (liftIO, MonadIO(..))
 import Control.Monad.Trans.Reader (ReaderT(..))
+import Control.Monad.IO.Unlift
+import Control.Monad.Logger
+import Control.Monad.Trans.Resource.Internal-- .ResourceT
+import Data.Aeson (ToJSON(..))
+import Data.Aeson (pairs, (.=))
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as BL
-import Data.Csv
+import Data.Csv hiding ((.=))
+-- import Data.Text hiding (map)
 import Data.Text.Lazy.Encoding (decodeUtf8)
-import qualified Data.Text.Lazy as T
+import Data.Text.Lazy (Text)
+-- import qualified Data.Text.Lazy as T
 import Data.Time.Format.ISO8601
 import Data.Time (UTCTime)
 import qualified Data.Vector as V
+-- import qualified Database.Esqueleto as E
+-- import Database.Esqueleto      ((^.))
 import Database.Persist
+-- import Database.Persist.Sql (rawSql)
 import Database.Persist.Sqlite
 import Database.Persist.TH
+import GHC.Generics
 import Web.Scotty (scotty)
 import qualified Web.Scotty as W
-
+-- import Yesod.Persist.Core (runDB)
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Person json
-    name String
+    name Text
     UniqueName name
     deriving Show
-Consumption
+Consumption json
     personId PersonId
     meat String
     time UTCTime
 |]
 
+data PersonDTO = PersonDTO
+    { pName :: Text
+    } deriving (Show, Generic)
+
+instance ToJSON PersonDTO where
+  toEncoding (PersonDTO name) =
+    pairs ("name" .= name)
+
 data ConsumptionDTO = ConsumptionDTO
-    { cdtoName :: !String
-    , cdtoBar  :: !String
-    , cdtoTime :: !UTCTime
-    }
+    { cName :: !Text
+    , cBar  :: !String
+    , cTime :: !UTCTime
+    } deriving (Show, Generic)
+
+instance ToJSON ConsumptionDTO where
+  toEncoding (ConsumptionDTO n b t) =
+    pairs ("name" .= n <> "bar" .= b <> "time" .= t)
 
 instance FromNamedRecord ConsumptionDTO where
     parseNamedRecord r = r .: "date" >>= \s -> case iso8601ParseM s of
@@ -62,27 +89,58 @@ instance FromNamedRecord ConsumptionDTO where
         <*> pure t
       Nothing -> fail "Invalid timestamp"
 
+runDB :: MonadUnliftIO m => ReaderT SqlBackend (NoLoggingT (ResourceT m)) a -> m a
+runDB f = runSqlite "meatbar.db" $ f
+
 insertConsumption :: MonadIO m => ConsumptionDTO -> ReaderT SqlBackend m (Key Consumption)
-insertConsumption c = do
-  res  <- insertBy $ Person $ cdtoName c
+insertConsumption (ConsumptionDTO n b t) = do
+  res  <- insertBy $ Person $ n
   pid <- case res of
     Left (Entity dup _) -> pure dup
     Right new           -> pure new
-  insert $ Consumption pid (cdtoBar c) (cdtoTime c)
+  insert $ Consumption pid b t
+
+getConsumptionsDB :: ReaderT SqlBackend (NoLoggingT (ResourceT IO))
+                   [(Entity Person, Entity Consumption)]
+getConsumptionsDB = rawSql "SELECT ??, ?? \
+  \FROM consumption INNER JOIN person \
+  \ON consumption.person_id=person.id"
+  []
+
+getPeople :: [(Entity Person)] -> [PersonDTO]
+getPeople es = map (\(Entity _ p) -> PersonDTO $ personName p) es
+
+getConsumptions :: [(Entity Person, Entity Consumption)] -> [ConsumptionDTO]
+getConsumptions es = map (\(p, c) ->
+  ConsumptionDTO (personName p) (consumptionMeat c) (consumptionTime c))
+  $ map (\((Entity _ p), (Entity _ c)) -> (p, c)) es
+
+-- data Response = Response
+--     { rName :: Text,
+--       rList :: [ToJSON]
+--     } deriving (Show, Generic)
+
+-- instance ToJSON Response where
+--   toEncoding (Response n l) =
+--     pairs (n .= l)
+
+-- respond :: Text -> [ToJSON] -> ByteString
+-- respond os = 
 
 main :: IO ()
-main = runSqlite ":memory:" $ do
-  runMigration migrateAll
+main = do
+  runDB $ runMigration migrateAll
 
   csv <- liftIO $ BL.readFile "data.csv"
   _ <- case decodeByName csv of
     Left err -> liftIO $ putStrLn err
     Right (_, v) -> V.forM_ v $ \ c -> do
-      insertConsumption c
+      runDB (insertConsumption c)
 
-  people <- selectList [] []
-  liftIO $ print (people :: [Entity Person])
-
-  liftIO $ scotty 3000 $ do
+  scotty 3000 $ do
     W.get "/people" $ do
-      W.text $ T.concat $ map (decodeUtf8 . A.encode ) (people :: [Entity Person])
+      ps <- liftIO $ runDB (selectList [] [])
+      W.text $ decodeUtf8 $ A.encode $ getPeople ps
+    W.get "/consumptions" $ do
+      cs <- liftIO $ runDB getConsumptionsDB
+      W.text $ decodeUtf8 $ A.encode $ getConsumptions cs
