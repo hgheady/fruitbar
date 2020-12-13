e55a4a624d1d1e9056e1ee3249d0b127ff15c61a
H. Gabriel Heady <headyhud@gmail.com>
2020-12-13 07:54:58 -0800

    FRUITBAR-0: Reorg using Lib, add test boilerplate

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,22 @@
+import Lib
+
+import Test.HUnit (assertEqual)
+import Test.Framework (defaultMain, Test, testGroup)
+import Test.Framework.Providers.HUnit (testCase)
+import Data.Monoid
+import Control.Monad
+-- import Utils
+
+testGroups :: [Test]
+testGroups = [ testFoo
+             ]
+
+--------------------------------------------------------------------------------
+testFoo :: Test
+testFoo = testGroup "Foo"
+    [ testCase "Foo test case" $ do
+        assertEqual "should not be possible" 1 1
+    ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain testGroups
